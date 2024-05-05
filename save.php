200f9f1cdd2216724e2568cbfce602b6cee1226d
Azamat <s.azamat0806@gmail.com>
2024-05-05 14:55:48 +0500

    Update save.php

--- save.php
+++ save.php
@@ -7,7 +7,7 @@ $message = $_POST["message"];
 $conn = mysqli_connect("localhost", "hp", "789789", "cloud_project") or die("connection failed");
 $sql = "INSERT INTO contact_form(First_name, Last_name, Email, Message) VALUES ('{$fistname}','{$lastname}','{$email}','{$message}' )";
 $result = mysqli_query($conn, $sql) or die("Query Failed!");
-echo "successfully ";
+echo "Data is successfully saved ";
 header("location: http://13.60.18.50/contacts.php");
 mysqli_close($conn);
 ?>
