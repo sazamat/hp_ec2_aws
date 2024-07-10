e5629408788fc1a237436834c68a5219c4bbe198
Azamat <s.azamat0806@gmail.com>
2024-07-10 18:51:04 +0200

    Update newsletter.php

--- newsletter.php
+++ newsletter.php
@@ -8,14 +8,12 @@ $email = $_POST["email"];
         
 $conn = mysqli_connect("localhost", "hp", "789789", "cloud_project") or die("connection failed");
         
-if (mysqli_connect_errno()) {
-    die("Connection error: " . mysqli_connect_error());
-}           
+
     
 
 $sql = "INSERT INTO newsletter(email) VALUES ('{$email}' )";
 $result = mysqli_query($conn, $sql) or die("Query Failed!");
-header("location: http://16.171.193.196/contacts.php");
+header("location: http://16.171.193.196//contacts.php");
 mysqli_close($conn);
 
 echo "Record saved.";
